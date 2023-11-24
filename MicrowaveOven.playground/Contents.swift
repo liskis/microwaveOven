929c7bc7fa180eedbb38cb84c5c8ba23f9b2a8d1
liskis <watanabe@liskis.org>
2023-11-24 10:35:26 +0900

    ファイルの中身を復元

--- MicrowaveOven.playground/Contents.swift
+++ MicrowaveOven.playground/Contents.swift
@@ -1,1 +1,33 @@
+import Foundation
+import PlaygroundSupport
 
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+class MicrowaveOven {
+    var timer: Timer?
+    var sec: Int = 0
+
+    func chin(sec: Int) {
+        print("あっためます。")
+        self.sec = sec
+        timer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(countdown),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+
+    @objc func countdown() {
+        sec -= 1
+        print("\(sec)")
+        if sec == 0 {
+            print("ちーん")
+            timer?.invalidate()
+        }
+    }
+}
+
+let microwaveOven = MicrowaveOven()
+microwaveOven.chin(sec: 5)
