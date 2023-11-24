3ce51e7c25127af34e7a81aa2280a4da7f17fa6b
liskis <watanabe@liskis.org>
2023-11-24 15:25:49 +0900

    温度を選べる機能を追加

--- MicrowaveOven.playground/Contents.swift
+++ MicrowaveOven.playground/Contents.swift
@@ -4,12 +4,46 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 class MicrowaveOven {
+    
+    enum Power {
+        case w100
+        case w200
+        case w500
+    }
+    
+    var maxTimeW100: Int = 1200
+    var maxTimeW200: Int = 900
+    var maxTimeW500: Int = 600
+    
     var timer: Timer?
-    var sec: Int = 0
+    var setTime: Int = 0
 
-    func chin(sec: Int) {
+    func start(power: Power, setTime: Int) -> Bool {
+        self.setTime = setTime
+        switch power {
+        case .w100:
+            if self.setTime >= maxTimeW100{
+                print("温め時間は\(maxTimeW100)以下でお願いします。")
+                return false
+            }
+        case .w200:
+            if self.setTime >= maxTimeW200{
+                print("温め時間は\(maxTimeW100)以下でお願いします。")
+                return false
+            }
+        case .w500:
+            if self.setTime >= maxTimeW500{
+                print("温め時間は\(maxTimeW100)秒以下でお願いします。")
+                return false
+            }
+        }
         print("あっためます。")
-        self.sec = sec
+        startTimer()
+        return true
+        
+    }
+    
+    func startTimer(){
         timer = Timer.scheduledTimer(
             timeInterval: 1,
             target: self,
@@ -20,14 +54,15 @@ class MicrowaveOven {
     }
 
     @objc func countdown() {
-        sec -= 1
-        print("\(sec)")
-        if sec == 0 {
-            print("ちーん")
+        setTime -= 1
+        print("\(setTime)")
+        if setTime == 0 {
+            print("上手にできました！")
             timer?.invalidate()
         }
     }
 }
 
 let microwaveOven = MicrowaveOven()
-microwaveOven.chin(sec: 5)
+let warmStart = microwaveOven.start(power: .w500, setTime: 800)
+print(warmStart)
